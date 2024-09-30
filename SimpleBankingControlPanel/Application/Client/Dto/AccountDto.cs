b780ca6d9657c082e6439d7e1f4ed2f95dd5cce3
Abdulmajeed Alrajhi <7d7om7@gmail.com>
2024-09-30 14:09:21 +0300

    fix: remove id

--- SimpleBankingControlPanel/Application/Client/Dto/AccountDto.cs
+++ SimpleBankingControlPanel/Application/Client/Dto/AccountDto.cs
@@ -2,7 +2,6 @@ namespace SimpleBankingControlPanel.Application.Client.Dto;
 
 public class AccountDto
 {
-    public Guid Id { get; set; }
     public string AccountNumber { get; set; }
 
     public decimal Balance { get; set; }
